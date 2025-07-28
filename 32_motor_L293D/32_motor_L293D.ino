32db85e366036d3ef3035ca4ef25dad5c26e63ce
profe-teo <colaboremos@gmail.com>
2025-07-28 12:20:30 +0200

    Lunes 28

--- 32_motor_L293D/32_motor_L293D.ino
+++ 32_motor_L293D/32_motor_L293D.ino
@@ -9,6 +9,10 @@
 #define DERECHA 1
 #define IZQUIERDA 2
 
+// Diodos LED
+#define ROJO 0
+#define VERDE 15
+#define AZUL 4
 
 
 void setup() {
@@ -17,6 +21,10 @@ void setup() {
   pinMode(VELOCIDAD, OUTPUT);
   pinMode(DIRPIN_1, OUTPUT);
   pinMode(DIRPIN_2, OUTPUT);
+
+  pinMode(ROJO,OUTPUT);
+  pinMode(VERDE,OUTPUT);
+  pinMode(AZUL,OUTPUT);
 
 }
 
@@ -37,7 +45,11 @@ void loop() {
 void cambioEstado(int estadoNuevo){
   switch(estadoNuevo){
     case PARO:
+      Serial.println("Motor parado.");
       analogWrite(VELOCIDAD, 0); // Aapagamos el motor
+      digitalWrite(ROJO,HIGH);
+      digitalWrite(AZUL,LOW);
+      digitalWrite(VERDE,LOW);
       break;
     case IZQUIERDA:
       //digitalWrite(VELOCIDAD, HIGH); // Encendemos el motor
@@ -45,13 +57,22 @@ void cambioEstado(int estadoNuevo){
       analogWrite(VELOCIDAD, 70);
       digitalWrite(DIRPIN_1, 0);
       digitalWrite(DIRPIN_2, 1);
+
+      digitalWrite(ROJO,LOW);
+      digitalWrite(AZUL,HIGH);
+      digitalWrite(VERDE,LOW);      
       break;
     case DERECHA:
       //digitalWrite(VELOCIDAD, HIGH); // Encendemos el motor
       Serial.println("Girando en la dirección DERECHA.");
       analogWrite(VELOCIDAD, 250);
       digitalWrite(DIRPIN_1, 1);
-      digitalWrite(DIRPIN_2, 0);    
+      digitalWrite(DIRPIN_2, 0); 
+
+      digitalWrite(ROJO,LOW);
+      digitalWrite(AZUL,LOW);
+      digitalWrite(VERDE,HIGH);   
+      break;      
   }
 
 
